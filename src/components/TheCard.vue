89346cb61bfe174b84a6cbed5e79a87493163f73
SeanBitter <paolodig@hotmail.it>
2022-06-29 17:52:18 +0200

    Add first part of Milestone 3

---
+++ src/components/TheCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <ul v-for="movie in moviesList" :key="movie.title">
+            <li>Titolo film: {{ movie.title }}</li>
+            <li>Titolo originale: {{ movie.original_title }}</li>
+            <li>Lingua originale: <lang-flag :iso="movie.original_language" /></li>
+            <li>Voto: {{ movie.vote_average }}</li>
+            <li><img :src="imgCover + movie.poster_path" :alt="movie.title"></li>
+        </ul>
+
+        <ul v-for="(tv) in seriesList" :key="tv.name">
+            <li>Titolo serie: {{ tv.name }}</li>
+            <li>Titolo originale: {{ tv.original_name }}</li>
+            <li>Lingua originale: <lang-flag :iso="tv.original_language" /></li>
+            <li>Voto: {{ tv.vote_average }}</li>
+            <li><img :src="imgCover + tv.poster_path" :alt="tv.title"></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {state} from '../store';
+
+export default {
+    data() {
+        return {
+            imgCover:'https://image.tmdb.org/t/p/w185/',
+        }
+    },
+
+    computed: {
+        moviesList() {
+            return state.moviesList;
+        },
+
+        seriesList() {
+            return state.seriesList;
+        },
+    },
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,34 +1,14 @@
 <template>
     <div>
-        <ul v-for="(movie) in moviesList" :key="movie.title">
-            <li>Titolo film: {{ movie.title }}</li>
-            <li>Titolo originale: {{ movie.original_title }}</li>
-            <li>Lingua originale: <lang-flag :iso="movie.original_language" /></li>
-            <li>Voto: {{ movie.vote_average }}</li>
-        </ul>
-
-        <ul v-for="(tv) in seriesList" :key="tv.name">
-            <li>Titolo serie: {{ tv.name }}</li>
-            <li>Titolo originale: {{ tv.original_name }}</li>
-            <li>Lingua originale: <lang-flag :iso="tv.original_language" /></li>
-            <li>Voto: {{ tv.vote_average }}</li>
-        </ul>
+        <TheCard></TheCard>
     </div>
 </template>
 
 <script>
-import {state} from '../store';
+import TheCard from './TheCard.vue';
 
 export default {
-    computed: {
-        moviesList() {
-            return state.moviesList;
-        },
-
-        seriesList() {
-            return state.seriesList;
-        },
-    },
+    components: { TheCard }
 }
 </script>
 
